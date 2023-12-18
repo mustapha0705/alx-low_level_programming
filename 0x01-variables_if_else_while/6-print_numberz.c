a09fc7bc6056d3d573cb35261e02f35949f75c67
Mustapha Mohammed <mustapha0705>
2023-12-18 14:57:43 -0800

    hm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always returns 0
+*/
+
+int main(void)
+{
+	putchar('\n' + ('0' - 48) * 9);
+	putchar('\n' + ('9' - 48) * 9);
+	return (0);
+}
