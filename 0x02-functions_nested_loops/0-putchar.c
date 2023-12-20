548953fdf7a511661262accd778d255a60e48d0e
Mustapha Mohammed <mustapha0705>
2023-12-20 13:10:20 -0800

    putchat

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*Return: Always returns 0
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
