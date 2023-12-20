1e3fd08024fdc1c0fd72901bf67979d1c0bbd5fe
Mustapha Mohammed <mustapha0705>
2023-12-20 14:04:52 -0800

    2nd function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints alphabets 10x
+*/
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
